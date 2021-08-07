6d6b3dbeb6482c21683fcb6d27412d390e358cf2
Gabriel Lemache <lemache35@gmail.com>
2021-08-06 22:49:48 -0300

    get data and treat

---
+++ stable_api.py
@@ -0,0 +1,44 @@
+import requests 
+
+class Api:
+    def __init__(self, quote, range, interval):
+        self.SESSION = requests.Session()
+        self.HEADER = {"User-Agent": r"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36"}
+        self.QUOTE = quote
+        self.RANGE = range
+        self.INTERVAL = interval
+    
+    def send_request(self):
+        url = f"https://query1.finance.yahoo.com/v8/finance/chart/{self.QUOTE}?region=US&lang=en-US&includePrePost=false&interval={self.INTERVAL}&useYfid=true&range={self.RANGE}&corsDomain=finance.yahoo.com&.tsrc=finance"
+        self.data = self.SESSION.get(url, headers=self.HEADER)
+        
+        return self.data
+   
+    def treat_data(self):
+        data = self.send_request().json()
+        print(data["chart"]["result"][0]["indicators"]["quote"][0]["high"])
+    
+    @property
+    def open(self):
+        data = self.send_request().json()
+        return data["chart"]["result"][0]["indicators"]["quote"][0]["open"]
+
+    @property
+    def close(self):
+        data = self.send_request().json()
+        return data["chart"]["result"][0]["indicators"]["quote"][0]["close"]
+
+    @property
+    def high(self):
+        data = self.send_request().json()
+        return data["chart"]["result"][0]["indicators"]["quote"][0]["high"]
+
+    @property
+    def low(self):
+        data = self.send_request().json()
+        return data["chart"]["result"][0]["indicators"]["quote"][0]["low"]
+
+
+
+
+print(Api("AMD", "5d", "1d").low)
